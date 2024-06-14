64ae81dcccac30ace6689c296a07ebfc34f4c808
Mateusz Konieczny <matkoniecz@gmail.com>
2024-06-14 10:06:12 +0200

    new reinstaller

--- reinstall.sh
+++ reinstall.sh
@@ -1,8 +1,11 @@
 rm dist -rf
 python3 setup.py sdist bdist_wheel
-cd dist
-pip3 uninstall example_pkg_matkoniecz -y
-pip3 install --user *.whl
-cd ..
+
+python3 ../../python_package_reinstaller/reinstaller.py example_pkg_matkoniecz # handles multiple python versions - please create an issue if missing code is a problem
+#cd dist
+#pip3 uninstall example_pkg_matkoniecz -y
+#pip3 install --user *.whl
+#cd ..
+
 python3 -m unittest
 # twine upload dist/* # to upload to PyPi
